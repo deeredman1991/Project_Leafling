2d41d5f76e42284effbdc7796334d6f116626300
deeredman1991 <deeredman1991@gmail.com>
2018-01-29 06:46:12 -0500

    fixed bug and naming issue with bowls

--- scripts/bowls.py
+++ scripts/bowls.py
@@ -7,14 +7,28 @@ from scripts.pile import Pile
 logger = logging.Logger
 
 class FoodBowl(Pile):
-    def __init__(self, name, contents_type, *args, **kwargs):
-        img = 'images/bowl.png'
+    def __init__(self, *args, **kwargs):
+        img = 'images/food_bowl.png'
 
         super(FoodBowl, self).__init__(*args,
                                    'Food Bowl',
                                    'food',
                                    max_contents = 100,
                                    source = img,
-                                   spillage_recepticle = Pile('Floor Pile',
+                                   spillage_recepticle = Pile('Spilled Food',
                                                               'food')
                                    **kwargs)
+
+class WaterBowl(Pile):
+    def __init__(self, *args, **kwargs):
+       img = 'images/water_bowl.png'
+
+       super(WaterBowl, self).__init__(*args,
+                                       'Water Bowl'
+                                       'water',
+                                       max_contents = 100,
+                                       source = img,
+                                       spillage_recepticle = Pile('Puddle of '\
+                                                                  'Water',
+                                                                  'water'),
+                                        **kwargs)
